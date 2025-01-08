ba504dc8e53382e3327739a84aa161bac78272c5
paolomorettin <morettin.paolo@gmail.com>
2025-01-08 17:46:48 +0100

    yup

--- experiments/synthetic_plot.sh
+++ experiments/synthetic_plot.sh
@@ -7,5 +7,8 @@ OUTPUT=$SYN_DIR/plots/
 
 mkdir -p $OUTPUT
 
-python3 plot.py $INPUT -o $OUTPUT -f r3-5_b0_d2-6  --legend-pos 4 # --timeout 200
-python3 plot.py $INPUT -o $OUTPUT -f r3-5_b0_d2-6_cactus --cactus --legend-pos 2 #--timeout 200
+#python3 plot.py $INPUT -o $OUTPUT -f r3-5_b0_d2-6  --legend-pos 4 # --timeout 200
+#python3 plot.py $INPUT -o $OUTPUT -f r3-5_b0_d2-6_cactus --cactus --legend-pos 2 #--timeout 200
+
+python3 plot.py $INPUT -o $OUTPUT -f r3_b0_d2-6  --legend-pos 4 # --timeout 200
+python3 plot.py $INPUT -o $OUTPUT -f r3_b0_d2-6_cactus --cactus --legend-pos 2 #--timeout 200
