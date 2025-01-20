51ca00e2767c6500a83563ae45010ff2a782d549
paolomorettin <morettin.paolo@gmail.com>
2025-01-20 17:42:53 +0100

    scatterplots: diagonals

--- experiments/latte_gasp_scatterplots.py
+++ experiments/latte_gasp_scatterplots.py
@@ -10,6 +10,7 @@ plt.style.use("ggplot")
 DEF_FOLDER = "latte_gasp/results/"
 INTEGRATORS = ["latte", "torch"]
 
+MIN_EXP = -2
 TIMEOUT_VAL = 3600
 MISSING_VAL = TIMEOUT_VAL
 
@@ -97,17 +98,21 @@ if __name__ == "__main__":
     ax.set_ylabel(YLABEL)
     ax.set_aspect("equal")
 
-    ax.set_xlim([min_val, TIMEOUT_VAL + 10**3])
-    ax.set_ylim([min_val, TIMEOUT_VAL + 10**3])
+    ax.set_xlim([10 ** MIN_EXP, TIMEOUT_VAL + 10**3])
+    ax.set_ylim([10 ** MIN_EXP, TIMEOUT_VAL + 10**3])
     ax.set_xscale("log")
     ax.set_yscale("log")
         
     ax.axhline(TIMEOUT_VAL, 0, TIMEOUT_VAL, linestyle="--", color=TIMEOUT_COLOR) # timeout lines and label
     ax.axvline(TIMEOUT_VAL, 0, TIMEOUT_VAL, linestyle="--", color=TIMEOUT_COLOR)
     ax.annotate("timeout", (0.02, 0.9), xycoords='axes fraction', color=TIMEOUT_COLOR)
+
+    ax.plot([10 ** MIN_EXP, TIMEOUT_VAL], [10 ** MIN_EXP, TIMEOUT_VAL], linestyle="--", color=DIAGONAL_COLOR) # diagonals
+    for i in range(1, 6):
+        ax.plot([10 ** (MIN_EXP + i), TIMEOUT_VAL], [10 ** MIN_EXP, 10**(np.log10(TIMEOUT_VAL) - i)], linestyle="--", color=DIAGONAL_COLOR, alpha=(1 - 0.1*i))
+        ax.plot([10 ** MIN_EXP, 10**(np.log10(TIMEOUT_VAL) - i)], [10 ** (MIN_EXP + i), TIMEOUT_VAL], linestyle="--", color=DIAGONAL_COLOR, alpha=(1 - 0.2*i))
     
     ax.scatter(xs, ys, marker=MARKER, color=COLOR, alpha=ALPHA)
-    ax.plot([min_val, TIMEOUT_VAL], [min_val, TIMEOUT_VAL], linestyle="--", color=DIAGONAL_COLOR) # diagonal
     plt.show()
             
 
